5aa7d4c15f586640af39b7bebf15bb3cf888592c
MaxCostaCardosoIFRO <contatomaxcostacardoso@gmail.com>
2025-05-12 10:05:20 -0400

    rotas atualizadas (teste)

---
+++ max/servicos/genero/adicionar.js
@@ -0,0 +1,27 @@
+import pool from '../../../config.js';
+
+async function executarQuery(sql, params = []) {
+    let conexao;
+    try {
+        conexao = await pool.getConnection();
+        const resultado = await conexao.execute(sql, params);
+        return resultado;
+    } catch (error) {
+        console.log(error)
+    } finally {
+        if (conexao) conexao.release();
+    }
+}
+
+async function adicionarGenero(classInd, dataLanc, sinopse, fotoFilme, nomeFilme) {
+    try{        
+        //console.log(nome, dataNasc, vivo, fotoAtor)
+        const sql = `INSERT INTO atores (nclassInd, dataLanc, sinopse, fotoFilme, nomeFilme) VALUES (?, ?, ?, ?);`;
+        return await executarQuery(sql, [classInd, dataLanc, sinopse, fotoFilme, nomeFilme]);
+    } catch(error) {
+        console.error(error);
+    }
+    
+}
+
+export { adicionarGenero }
